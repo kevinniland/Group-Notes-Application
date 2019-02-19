671a4bfc8b6b7797b2e3d51a60bbc9479ec2d010
Matthew Sloyan <g00348036@gmit.ie>
2019-02-19 17:18:36 +0000

    Installed Camera plugin, and added the basic ability to take a picture. This will be updated to save to database.

--- GroupNotesApp/src/app/home/home.module.ts
+++ GroupNotesApp/src/app/home/home.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { HomePage } from './home.page';
 
 @NgModule({
@@ -20,4 +20,33 @@ import { HomePage } from './home.page';
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule {
+
+  constructor(private camera: Camera) {}
+
+  base64Image;
+
+  //open the camera on mobile devices to take a picture, 
+  //this will allow the user to crop it to a 1:1 aspect ration (Square) and then save it for testing
+  openCamera(){
+    const options: CameraOptions = {
+      quality: 75,
+      allowEdit : true,
+      targetWidth: 300,
+      targetHeight: 300,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    //get the picture taken and store it as a base64 image.
+    this.camera.getPicture(options).then((imageData) => {
+
+     // imageData is either a base64 encoded string or a file URI
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
+
+    }, (err) => {
+      console.log("Error");
+    });
+  }
+}
