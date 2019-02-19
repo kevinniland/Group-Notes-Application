466418aba97827a0a456e558258ca381222c5d98
Matthew Sloyan <g00348036@gmit.ie>
2019-02-19 21:05:48 +0000

    Fixed issue with opening camera, the user can now take a picture and crop it.

--- GroupNotesApp/src/app/home/home.module.ts
+++ GroupNotesApp/src/app/home/home.module.ts
@@ -22,31 +22,4 @@ import { HomePage } from './home.page';
 })
 export class HomePageModule {
 
-  constructor(private camera: Camera) {}
-
-  base64Image;
-
-  //open the camera on mobile devices to take a picture, 
-  //this will allow the user to crop it to a 1:1 aspect ration (Square) and then save it for testing
-  openCamera(){
-    const options: CameraOptions = {
-      quality: 75,
-      allowEdit : true,
-      targetWidth: 300,
-      targetHeight: 300,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
-    
-    //get the picture taken and store it as a base64 image.
-    this.camera.getPicture(options).then((imageData) => {
-
-     // imageData is either a base64 encoded string or a file URI
-     this.base64Image = 'data:image/jpeg;base64,' + imageData;
-
-    }, (err) => {
-      console.log("Error");
-    });
-  }
 }
--- GroupNotesApp/src/app/home/home.page.ts
+++ GroupNotesApp/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private camera: Camera) {}
 
+  base64Image;
+
+  //open the camera on mobile devices to take a picture, 
+  //this will allow the user to crop it to a 1:1 aspect ration (Square) and then save it for testing
+  openCamera(){
+    console.log("Hello");
+    const options: CameraOptions = {
+      quality: 75,
+      allowEdit : true,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    //get the picture taken and store it as a base64 image.
+    this.camera.getPicture(options).then((imageData) => {
+
+      // imageData is either a base64 encoded string or a file URI
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+
+    }, (err) => {
+      console.log("Error");
+    });
+  }
 }
