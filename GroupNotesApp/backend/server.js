ed9defea7eed8d3cc7d2ea9d423c2d3a7afe61d8
Matthew Sloyan <g00348036@gmit.ie>
2019-02-22 19:58:40 +0000

    Updated server.jswith storage credentials, however it still contains errors. Must fix.

--- GroupNotesApp/backend/server.js
+++ GroupNotesApp/backend/server.js
@@ -1,9 +1,5 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var bodyParser = require('body-parser');
-
-// NodeJS Server Setup
+//NODEJS SERVER SETUP 
+//===============
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -17,8 +13,7 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS")
-    res.header("Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
     
@@ -54,18 +49,6 @@ var userSchema = new Schema ({
 
 var UserModel = database.model('users', userSchema);
 
-// Here we are configuring express to use body-parser as middle-ware 
-app.use(bodyParser.urlencoded({ extended: false })); 
-app.use(bodyParser.json());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-    });
-
-
 app.get('/', function (req, res) {
     res.send('Connected to server');
 })
@@ -92,9 +75,41 @@ app.get('/api/users', function (req, res) {
     });
 })
 
-var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
-    
-    console.log("GroupNotesApp listening at http://%s:%s", host, port)
- })
+//FIREBASE SETUP (Storage)
+//================
+var firebase = require("firebase");
+
+var config = {
+  apiKey: 'AIzaSyCEQtQ7Pu_BlxZxkW8ssimqRjHIyySkOnM',
+  authDomain: 'groupnotesapp.firebaseapp.com',
+  databaseURL: 'https://groupnotesapp.firebaseio.com',
+  storageBucket: 'gs://groupnotesapp.appspot.com'
+};
+
+firebase.initializeApp(config);
+
+// Get a reference to the storage service, which is used to create references in the storage bucket
+var storage = firebase.storage();
+
+// Create a storage reference
+var storageRef = storage.ref();
+var testRef = storageRef.child('test.jpg');
+
+// Create a reference to 'images/test.jpg'
+var testImagesRef = storageRef.child('images/test.jpg');
+
+//var file =  // use the Blob or File API
+
+//POST method which console logs data passed up to the server
+app.post('/api/files', function (req, res) {
+
+  testImagesRef.put(req).then(function(snapshot) {
+    console.log('Uploaded File!');
+  });
+
+  //res.status(201).json({message: "Image Uploaded"});
+})
+
+
+
+
