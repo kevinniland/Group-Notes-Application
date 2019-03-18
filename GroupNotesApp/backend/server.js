7ec3189c85805ea4bb71a116814b6b25c55b37a8
Kevin Niland <G00342279@gmit.ie>
2019-03-18 21:03:17 +0000

    Group chat will be implemented via socket.io -- socket.io installed and server side of group chat started

--- GroupNotesApp/backend/server.js
+++ GroupNotesApp/backend/server.js
@@ -99,6 +99,9 @@ app.get('/api/users/:id', function(req, res) {
         })
 })
 
+// Socket.io setup
+// ============================
+
 var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
