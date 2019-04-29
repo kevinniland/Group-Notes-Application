b30c7554c58f3b484e6599136b871e3fe4f7f13b
Kevin Niland <G00342279@gmit.ie>
2019-04-29 11:34:52 +0100

    Removed 'View User' - User profile page not implemented

--- GroupNotesApp/src/app/home/home.module.ts
+++ GroupNotesApp/src/app/home/home.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { HomePage } from './home.page';
 
 @NgModule({
